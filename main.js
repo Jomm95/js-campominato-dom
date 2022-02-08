b8c46abe70c82b30ef1bd231b603bf7a9b5941b8
Thomas <venturini.thomas95@gmail.com>
2022-02-09 00:57:11 +0100

    da completare ma impossibile

--- main.js
+++ main.js
@@ -1,36 +1,43 @@
-/*qua metterò un addEventListener su tasto play 
-per generare il giusto numero di box nella griglia*/
-document.getElementById("play").addEventListener("click", game);
+    //funzione che fa partire il gioco, premendo play parte la funzione game
+    document.getElementById("play").addEventListener("click", game);
+
+
 
  //funzione che gestisce il gioco
 function game () {
 
     const difficult = document.getElementById("difficult").value;
 
-    let numberBox;
-    let order;
+        let numberBox;
+        const bombNumber = 16;
+        console.log(bombNumber);
 
-    switch (difficult) {
-        case "easy":
-        default:
-            numberBox = 100;
-            break;
-        case "medium":
-            numberBox = 81;
-            break;
-        case "crazy":
-            numberBox = 49;
-            break;
-    }
-            
-
-    //creo un riferimento alla griglia per generare i box all'interno
-    let gridBox = document.getElementById("grid-container");
-    //resetto il campo da gioco 
-    gridBox.innerHTML = "";
-
+        switch (difficult) {
+            case "easy":
+            default:
+                numberBox = 100;
+                break;
+            case "medium":
+                numberBox = 81;
+                break;
+            case "crazy":
+                numberBox = 49;
+                break;
+        }
         order = Math.sqrt(numberBox);
 
+        gridGenerator();
+        const bombe = bombGenerator(16, numberBox);
+
+
+
+    //creo funzione che genera la griglia
+    function gridGenerator() {
+        let gridBox = document.getElementById("grid-container");
+        gridBox.innerHTML = "";
+        order = Math.sqrt(numberBox);
+
+        //ciclo for che crea gli square
         for (let i = 1; i <= numberBox; i++) {
             
         
@@ -49,45 +56,45 @@ function game () {
             //li aggiungo tutti nella gridBox
             gridBox.appendChild(square);
         }
-
         return true;
+    }
+
+    //creo una funzione per creare 16 celle di bombe
+    function bombGenerator (bombNumber, numberBox) {
+        
+        const bombsGenerated = [];
+        while (bombsGenerated.length < bombNumber) {
+            const bomb = getRandomNumber(1, numberBox);
+
+            if (!bombsGenerated.includes(bomb)) {
+                bombsGenerated.push(bomb);
+            }
+            console.log(bombsGenerated);
+        }
+        return bombsGenerated;
 
     }
 
-    function notCellClick() {
+
+    //funzione che crea numeri casuali
+    function getRandomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    //funzione che gestisce il click
+     function notCellClick() {
         this.classList.add("colorSafe");
         this.removeEventListener("click", notCellClick);
-    }
 
-    
-        
-        
+        const cell =parseInt(this.innertext) ;
+        if (bombe.includes(cell)) {
+            alert("hai preso una bomba");
+            this.classList.remove("colorSafe");
+            this.classList.add("colorBomb");
+        } else {
 
-        
-
-
-    
-//creo una funzione per creare 16 celle di bombe
-function bombGenerator (bombNumber, numberBox) {
-    const bombsGenerated = [];
-    while (bombsGenerated.length < bombNumber) {
-        const bomb = getRandomNumber(1, numberBox);
-
-        if (!bombsGenerated.includes(bomb)) {
-            bombsGenerated.push(bomb);
         }
     }
-    return bombsGenerated;
+    
+} //fine funzione game
 
-}
-
-
-
-
-
-
-
-//funzione che crea numeri casuali
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
